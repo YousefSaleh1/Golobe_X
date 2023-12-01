52a3651240ab7b204db69b003b3a29118ee1fafa
YousefSaleh1 <ys9882111@gmail.com>
2023-12-01 18:10:56 +0200

    Sanctum && AuthController && apiresponseTrait && UplodePhotoTrait

--- Golobe_X/config/filesystems.php
+++ Golobe_X/config/filesystems.php
@@ -44,6 +44,16 @@ return [
             'throw' => false,
         ],
 
+        'Focal_images'=>[
+            'driver' => 'local',
+            'root' => public_path('photos'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+
+         ],
+
+         
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
